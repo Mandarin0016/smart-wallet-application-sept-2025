a23836bfad82c5900ec38166cc982979563c8a00
Viktor Aleksandrov <viktorostrov07@gmail.com>
2025-09-15 22:09:32 +0300

    Initial commit

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,14 @@
+package app;
+
+import app.user.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
