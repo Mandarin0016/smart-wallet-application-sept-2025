e679879ea0ae6810798068e2c897a437fc7fd454
Viktor Aleksandrov <viktorostrov07@gmail.com>
2025-11-26 11:19:07 +0200

    aop

--- src/test/java/app/SubscribeITest.java
+++ src/test/java/app/SubscribeITest.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -26,7 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-//@ActiveProfiles("test")
+@ActiveProfiles("test")
 @SpringBootTest
 public class SubscribeITest {
 
